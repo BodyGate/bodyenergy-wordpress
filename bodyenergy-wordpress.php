545ac9775a7a83824944fb455f678d0afef18b67
BodyGate <gpisacan9522@gmail.com>
2026-07-13 14:54:02 +0200

    Add initial Body Energy WordPress plugin

---
+++ bodyenergy-wordpress.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Body Energy Integration
+ * Description: Plugin base per le integrazioni tra il sito WordPress Body Energy ASD e BodyGate.
+ * Version: 0.1.0
+ * Author: Body Energy ASD
+ * Text Domain: bodyenergy-wordpress
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('BODYENERGY_WORDPRESS_VERSION', '0.1.0');
+define('BODYENERGY_WORDPRESS_FILE', __FILE__);
+define('BODYENERGY_WORDPRESS_PATH', plugin_dir_path(__FILE__));
