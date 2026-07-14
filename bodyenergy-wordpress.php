486c9dfa35b6f414410aa0548433d01213f4b26b
BodyGate <gpisacan9522@gmail.com>
2026-07-14 16:28:32 +0200

    Bump plugin to 0.6.0 for direct verified Home video

--- bodyenergy-wordpress.php
+++ bodyenergy-wordpress.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Body Energy Integration
  * Description: Plugin base per le integrazioni tra il sito WordPress Body Energy ASD e BodyGate.
- * Version: 0.5.9
+ * Version: 0.6.0
  * Author: Body Energy ASD
  * Text Domain: bodyenergy-wordpress
  */
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('BODYENERGY_WORDPRESS_VERSION', '0.5.9');
+define('BODYENERGY_WORDPRESS_VERSION', '0.6.0');
 define('BODYENERGY_WORDPRESS_FILE', __FILE__);
 define('BODYENERGY_WORDPRESS_PATH', plugin_dir_path(__FILE__));
 
