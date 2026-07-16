3ba7302a5efdd5aec22c8b0578da9d8b48d2fce2
BodyGate <gpisacan9522@gmail.com>
2026-07-16 12:59:40 +0200

    Bump plugin to 0.6.4 for Pilates capacity correction

--- bodyenergy-wordpress.php
+++ bodyenergy-wordpress.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Body Energy Integration
  * Description: Plugin base per le integrazioni tra il sito WordPress Body Energy ASD e BodyGate.
- * Version: 0.6.3
+ * Version: 0.6.4
  * Author: Body Energy ASD
  * Text Domain: bodyenergy-wordpress
  */
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('BODYENERGY_WORDPRESS_VERSION', '0.6.3');
+define('BODYENERGY_WORDPRESS_VERSION', '0.6.4');
 define('BODYENERGY_WORDPRESS_FILE', __FILE__);
 define('BODYENERGY_WORDPRESS_PATH', plugin_dir_path(__FILE__));
 
